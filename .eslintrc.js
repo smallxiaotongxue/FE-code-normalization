e6511f23b5df497a311f662b34e63cd18e32b260
smallxiaotongxue <583883731@qq.com>
2021-05-02 12:47:27 +0800

    feat: add eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,19 +2,19 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true
+    node: true,
   },
   extends: [
     'plugin:vue/essential',
     'airbnb-base',
-    'plugin:prettier/recommended' // 添加 prettier 插件
   ],
   parserOptions: {
     ecmaVersion: 12,
-    parser: '@typescript-eslint/parser',
-    sourceType: 'module'
+    sourceType: 'module',
   },
-  plugins: ['vue', '@typescript-eslint'],
-  rules: {}
-}
-
+  plugins: [
+    'vue',
+  ],
+  rules: {
+  },
+};
